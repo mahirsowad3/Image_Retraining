6ee91cc9ac7bf2332c12844142a1fc9d2d311b24
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2018-12-29 21:19:33 -0500

    Testing with only one class

--- Training/2_generate_tfrecords.py
+++ Training/2_generate_tfrecords.py
@@ -170,12 +170,8 @@ def classAsTextToClassAsInt(classAsText):
     # ToDo: If you have more than one classification, add an if statement for each
     # ToDo: i.e. if you have 3 classes, you would have 3 if statements and then the else
 
-    if classAsText == 'bed':
+    if classAsText == 'traffic_light':
         return 1
-    if classAsText == 'car':
-        return 2
-    if classAsText == 'chair':
-        return 3
     else:
         print("error in class_text_to_int(), row_label could not be identified")
         return -1
