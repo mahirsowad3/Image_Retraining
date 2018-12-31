573a0353b852c87247f488f778b60793926a3b96
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2018-12-31 02:33:29 -0500

    Changes to name file in increments based on download success

--- Image Gathering/download_from_link.py
+++ Image Gathering/download_from_link.py
@@ -5,21 +5,26 @@ import datetime
 
 file = open('bed_links.txt', 'r')
 
+images_successfully_downloaded = 1
 
-def download_file(url, file_name):
+
+def download_file(url):
+    global  images_successfully_downloaded
     try:
+        file_name = str(images_successfully_downloaded)
         print('Beginning download of ' + file_name + '.jpg')
         urllib.request.urlretrieve(url, os.getcwd() + '/ImgNet Images/bed/' + file_name + '.jpg')
         print('Done for ' + file_name)
+        images_successfully_downloaded += 1
     except:
         print('image download did not work')
 
 
 start_time = time.time()
-num_image = 0
+num_link = 0
 for i in file.readlines():
-    num_image += 1
-    download_file(i, str(num_image))
+    num_link += 1
+    download_file(i)
 
 end_time = time.time()
 completion_time = end_time - start_time
