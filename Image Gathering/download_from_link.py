c947fd73074d090db2347508bfb00d3f8740f1bc
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2018-12-31 02:21:40 -0500

    Added time completion tracking and individualized links file

--- Image Gathering/download_from_link.py
+++ Image Gathering/download_from_link.py
@@ -1,7 +1,9 @@
 import os
 import urllib.request
+import time
+import datetime
 
-file = open('links.txt', 'r')
+file = open('bed_links.txt', 'r')
 
 
 def download_file(url, file_name):
@@ -13,9 +15,13 @@ def download_file(url, file_name):
         print('image download did not work')
 
 
+start_time = time.time()
 num_image = 0
 for i in file.readlines():
     num_image += 1
     download_file(i, str(num_image))
 
+end_time = time.time()
+completion_time = end_time - start_time
+print('Time completion: ' + str(datetime.timedelta(seconds=completion_time)))
 
